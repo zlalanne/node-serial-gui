0741527b73802f2d38fa934f3ec9d0e504730024
Zack Lalanne <zack.lalanne@ti.com>
2014-02-23 12:21:45 -0600

    Added simple fw to print random numbers to serial

---
+++ fw/fw.ino
@@ -0,0 +1,14 @@
+
+void setup() { 
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600); 
+} 
+
+
+void loop() { 
+
+  uint8_t value = random(0,100);
+  Serial.write(value);
+  delay(2000);
+  
+}
